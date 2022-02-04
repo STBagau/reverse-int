7ade702d170326b52677f9b05d3c277c4a4ed76b
STBagau <stanisbagau@gmail.com>
2022-02-04 23:05:02 +0100

    add: task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
+const { src } = require("semver");
+
 module.exports = function reverse (n) {
-  
+    
+    let i = 1;
+    let result = '';
+    let numberAbs = Math.abs(n);
+    let number = String(numberAbs);
+    
+    while (i <= number.length) {
+        result = `${result}${number[number.length - i]}`
+        i = i + 1
+    };
+
+    resultNumber = Number(result);
+
+    return resultNumber;
+
+
+
 }
+
